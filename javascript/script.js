c46b4a7438b156857fa01749f924cd3366630559
Technophile <patricia.snodgrass@gmail.com>
2018-05-08 00:15:28 -0500

    show display name in account details

--- javascript/script.js
+++ javascript/script.js
@@ -232,7 +232,9 @@ function createMarker(place) {
             // [START_EXCLUDE]
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
             document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            // document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            document.getElementById('quickstart-account-details').textContent = displayName;
+
             // [END_EXCLUDE]
           } else {
             // User is signed out.
@@ -240,7 +242,7 @@ function createMarker(place) {
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
             document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
             document.getElementById('quickstart-account-details').textContent = 'null';
-            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+            // document.getElementById('quickstart-oauthtoken').textContent = 'null';
             // [END_EXCLUDE]
           }
           // [START_EXCLUDE]
