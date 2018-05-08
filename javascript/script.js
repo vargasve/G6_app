094ac9798af712fbd80ce239d69e2df2fcab6c32
Technophile <patricia.snodgrass@gmail.com>
2018-05-07 23:51:39 -0500

    added platform.js cdn

--- javascript/script.js
+++ javascript/script.js
@@ -166,12 +166,7 @@ function createMarker(place) {
           // [END signin]
         } else {
           // [START signout]
-          function signOut() {
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-              console.log('User signed out.');
-            });
-          }
+          firebase.auth().signOut();
           // [END signout]
         }
         // [START_EXCLUDE]
