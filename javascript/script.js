d117cf4431b5d0af677527f586e87e76ebb39c97
Technophile <patricia.snodgrass@gmail.com>
2018-05-07 19:19:37 -0500

    added platform.js cdn

--- javascript/script.js
+++ javascript/script.js
@@ -149,118 +149,110 @@ function createMarker(place) {
 // Google OAuth
 
 
-
-
-
-        // [START googlecallback]
-        function onSignIn(googleUser) {
-            console.log('Google Auth Response', googleUser);
-            // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-            var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-              unsubscribe();
-              // Check if we are already signed-in Firebase with the correct user.
-              if (!isUserEqual(googleUser, firebaseUser)) {
-                // Build Firebase credential with the Google ID token.
-                // [START googlecredential]
-                var credential = firebase.auth.GoogleAuthProvider.credential(
-                    googleUser.getAuthResponse().id_token);
-                // [END googlecredential]
-                // Sign in with credential from the Google user.
-                // [START authwithcred]
-                firebase.auth().signInWithCredential(credential).catch(function(error) {
-                  // Handle Errors here.
-                  var errorCode = error.code;
-                  var errorMessage = error.message;
-                  // The email of the user's account used.
-                  var email = error.email;
-                  // The firebase.auth.AuthCredential type that was used.
-                  var credential = error.credential;
-                  // [START_EXCLUDE]
-                  if (errorCode === 'auth/account-exists-with-different-credential') {
-                    alert('You have already signed up with a different auth provider for that email.');
-                    // If you are using multiple auth providers on your app you should handle linking
-                    // the user's accounts here.
-                  } else {
-                    console.error(error);
-                  }
-                  // [END_EXCLUDE]
-                });
-                // [END authwithcred]
-              } else {
-                console.log('User already signed-in Firebase.');
-              }
-            });
+  /**
+     * Function called when clicking the Login/Logout button.
+     */
+    // [START buttoncallback]
+    function toggleSignIn() {
+        if (!firebase.auth().currentUser) {
+          // [START createprovider]
+          var provider = new firebase.auth.GoogleAuthProvider();
+          // [END createprovider]
+          // [START addscopes]
+          provider.addScope('https://www.googleapis.com/auth/plus.login');
+          // [END addscopes]
+          // [START signin]
+          firebase.auth().signInWithRedirect(provider);
+          // [END signin]
+        } else {
+          // [START signout]
+          firebase.auth().signOut();
+          // [END signout]
+        }
+        // [START_EXCLUDE]
+        document.getElementById('quickstart-sign-in').disabled = true;
+        // [END_EXCLUDE]
+      }
+      // [END buttoncallback]
+  
+  
+      /**
+       * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+       *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+       *    out, and that is where we update the UI.
+       *  - firebase.auth().getRedirectResult(): This promise completes when the user gets back from
+       *    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
+       */
+      function initApp() {
+        // Result from Redirect auth flow.
+        // [START getidptoken]
+        firebase.auth().getRedirectResult().then(function(result) {
+          if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-oauthtoken').textContent = token;
+          } else {
+            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+            // [END_EXCLUDE]
           }
-          // [END googlecallback]
-      
-          /**
-           * Check that the given Google user is equals to the given Firebase user.
-           */
-          // [START checksameuser]
-          function isUserEqual(googleUser, firebaseUser) {
-            if (firebaseUser) {
-              var providerData = firebaseUser.providerData;
-              for (var i = 0; i < providerData.length; i++) {
-                if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-                    providerData[i].uid === googleUser.getBasicProfile().getId()) {
-                  // We don't need to reauth the Firebase connection.
-                  return true;
-                }
-              }
-            }
-            return false;
+          // The signed-in user info.
+          var user = result.user;
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+            // If you are using multiple auth providers on your app you should handle linking
+            // the user's accounts here.
+          } else {
+            console.error(error);
           }
-          // [END checksameuser]
-      
-          /**
-           * Handle the sign out button press.
-           */
-          function handleSignOut() {
-            var googleAuth = gapi.auth2.getAuthInstance();
-            googleAuth.signOut().then(function() {
-              firebase.auth().signOut();
-            });
+          // [END_EXCLUDE]
+        });
+        // [END getidptoken]
+  
+        // Listening for auth state changes.
+        // [START authstatelistener]
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            // [END_EXCLUDE]
+          } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+            // [END_EXCLUDE]
           }
-      
-      
-          /**
-           * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-           *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-           *    out, and that is where we update the UI.
-           */
-          function initApp() {
-            // Auth state changes.
-            // [START authstatelistener]
-            firebase.auth().onAuthStateChanged(function(user){
-              if (user) {
-                // User is signed in.
-                var displayName = user.displayName;
-                var email = user.email;
-                var emailVerified = user.emailVerified;
-                var photoURL = user.photoURL;
-                var isAnonymous = user.isAnonymous;
-                var uid = user.uid;
-                var providerData = user.providerData;
-                // [START_EXCLUDE]
-                document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-                document.getElementById('signout').disabled = false;
-                document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-                // [END_EXCLUDE]
-              } else {
-                // User is signed out.
-                // [START_EXCLUDE]
-                document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-                document.getElementById('signout').disabled = true;
-                document.getElementById('quickstart-account-details').textContent = 'null';
-                  // [END_EXCLUDE]
-              }
-            });
-            // [END authstatelistener]
-      
-            document.getElementById('signout').addEventListener('click', handleSignOut, false);
-          }
-      
-          window.onload = function() {
-            initApp();
-          };
-    
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authstatelistener]
+  
+        document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+      }
+  
+      window.onload = function() {
+        initApp();
+      };
