2890cdf629730db83ad618d55d7ca01bb66933ea
Technophile <patricia.snodgrass@gmail.com>
2018-05-09 00:23:28 -0500

    added new maps files

--- javascript/script.js
+++ javascript/script.js
@@ -14,6 +14,77 @@ google.maps.event.addDomListener(window, 'load', function () {
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: austin,
     zoom: 15,
+    styles: [
+      {
+          "featureType": "landscape.natural",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#e0efef"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "hue": "#1900ff"
+              },
+              {
+                  "color": "#c0e8e8"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "lightness": 700
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#7dcdcd"
+              }
+          ]
+      }
+  ],
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
