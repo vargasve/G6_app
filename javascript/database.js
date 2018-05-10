9443a4789094ea2ae9f8f8752ba0c91269aca45b
Technophile <patricia.snodgrass@gmail.com>
2018-05-10 09:14:19 -0500

    merge

--- javascript/database.js
+++ javascript/database.js
@@ -38,16 +38,16 @@ $(document).ready(function () {
 
     //////////////////////////////////////////////////
     // Buttons
-
-    var beer = $("<button>").attr("id", "btn-beer").addClass("button btn1").html("<i class='flaticon-beer'></i>");
-    var wine = $("<button>").attr("id", "btn-wine").addClass("button btn2").html("<i class='flaticon-wine-glass'></i>");
-    var food = $("<button>").attr("id", "btn-food").addClass("button btn3").html("<i class='flaticon-food'></i>");
-    var hookah = $("<button>").attr("id", "btn-hookah").addClass("button btn4").html("<i class='flaticon-hookah'></i>");
-    var dog = $("<button>").attr("id", "btn-dog").addClass("button btn5").html("<i class='flaticon-dog'></i>");
-
-    var buttons = $("<div>").addClass("buttons-wrap").append(beer, wine, food, hookah, dog);
-    $("#hhInfo").append(buttons);
-
+    /*
+        var beer = $("<button>").attr("id", "btn-beer").addClass("button btn1").html("<i class='flaticon-beer'></i>");
+        var wine = $("<button>").attr("id", "btn-wine").addClass("button btn2").html("<i class='flaticon-wine-glass'></i>");
+        var food = $("<button>").attr("id", "btn-food").addClass("button btn3").html("<i class='flaticon-food'></i>");
+        var hookah = $("<button>").attr("id", "btn-hookah").addClass("button btn4").html("<i class='flaticon-hookah'></i>");
+        var dog = $("<button>").attr("id", "btn-dog").addClass("button btn5").html("<i class='flaticon-dog'></i>");
+    
+        var buttons = $("<div>").addClass("buttons-wrap").append(beer, wine, food, hookah, dog);
+        $("#hhInfo").append(buttons);
+    */
     $("#submit").on("click", function (event) {
         event.preventDefault();
 
@@ -57,41 +57,49 @@ $(document).ready(function () {
         var hhID = $("#place-id").val().trim();
         var hhStart = $("#place-hh-start").val().trim();
         var hhEnd = $("#place-hh-end").val().trim();
-        var hhBeer = $("#input-beer").val();
-        var hhWine = $("#input-wine").val();
-        var hhFood = $("#input-food").val();
-        var hhDog = $("#input-dog").val();
-        var hhHookah = $("#input-hookah").val();
-        var hhComments = $("#place-notes").val();
+        var hhBeer = $("#input-beer").prop("checked");
+        var hhWine = $("#input-wine").prop("checked");
+        var hhFood = $("#input-food").prop("checked");
+        var hhDog = $("#input-dog").prop("checked");
+        var hhHookah = $("#input-hookah").prop("checked");
+        var hhComment = $("#place-notes").val();
 
         //////////////////////////////////////////////////
         // Create object to hold filter data
 
-        var hhFeatures = {};
-        hhFeatures.featureData = {
-            "hhPlaceID": {
-                "hhTimes": {
-                    "starttime": "",
-                    "endtime": "",
-                },
-                "hhFilters": {
-                    "beer": false,
-                    "wine": false,
-                    "food": false,
-                    "hookah": false,
-                    "dog": false,
-                },
-                "hhComments": "",
-            }
-        }
+        var hhPlaceData = {
+            "comment" : hhComment,
+            "features" : {
+                "beer"   : !!hhBeer,
+                "wine"   : !!hhWine,
+                "food"   : !!hhFood,
+                "hookah" : !!hhHookah,
+                "dog"    : !!hhDog,
+        },
+            "times" : {
+                "starttime" : hhStart,
+                "endtime"   : hhEnd,
+            },
+        };
 
-        database.ref("/happyHowlerData").push(hhFeatures.featureData);
+        database.ref("/happyHowlerData/places").child(hhID).set(hhPlaceData);
+    })
 
-        console.log(hhFeatures.featureData);
+
+    database.ref("/happyHowlerData/places").on("child_added", function (childSnapshot) {
+        var hhID = childSnapshot.key;
+        var hhStart = childSnapshot.val().times.starttime;
+        var hhEnd = childSnapshot.val().times.endtime;
+        var hhBeer = childSnapshot.val().features.beer;
+        var hhWine = childSnapshot.val().features.wine;
+        var hhFood = childSnapshot.val().features.food;
+        var hhDog = childSnapshot.val().features.hookah;
+        var hhHookah = childSnapshot.val().features.dog;
+        var hhComment = childSnapshot.val().comment;
 
         /////////////////////////////////////////////////
         // Add to personal data table
-        var newRow = $("<tr>");
+        var newRow = $("<tr>").attr("id", "hhrow-" + hhID);
         var hhIDDisplay = $("<td>").text(hhID);
         var hhStartDisplay = $("<td>").text(hhStart);
         var hhEndDisplay = $("<td>").text(hhEnd);
@@ -100,28 +108,12 @@ $(document).ready(function () {
         var hhFoodDisplay = $("<td>").text(hhFood);
         var hhDogDisplay = $("<td>").text(hhDog);
         var hhHookahDisplay = $("<td>").text(hhHookah);
-        var hhCommentsDisplay = $("<td>").text(hhComments);
+        var hhCommentsDisplay = $("<td>").text(hhComment);
 
         newRow.append(hhIDDisplay, hhStartDisplay, hhEndDisplay, hhBeerDisplay, hhWineDisplay, hhFoodDisplay, hhDogDisplay, hhHookahDisplay, hhCommentsDisplay);
         $("#hhdata").append(newRow);
 
-    })
-
-
-    database.ref("/happyHowlerData").on("child_added", function(childSnapshot) {
-        var hhID = childSnapshot.val().hhPlaceID;
-        var hhStart = childSnapshot.val().starttime;
-        var hhEnd = childSnapshot.val().endtime;
-        var hhBeer = childSnapshot.val().beer;
-        var hhWine = childSnapshot.val().wine;
-        var hhFood = childSnapshot.val().food;
-        var hhDog = childSnapshot.val().hookah;
-        var hhHookah = childSnapshot.val().dog;
-        var hhComments = childSnapshot.val().hhComments;
-
-    })
-
-
+    });
 });
 
 
