260d249a23a6ad8443a0b43738b52b8def7ee38f
Technophile <patricia.snodgrass@gmail.com>
2018-05-13 02:51:31 -0500

    merge

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,20 @@
 // Grabbing and populating favorites based on map boundaries
 
 var searchwords = "happy+hour";
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var labelIndex = 0;
 var markers = [];
 var infoWindow;
-
 var austin = { lat: 30.2672, lng: -97.7431 };
 
-// Initializes the map and its styles
+//////////////////////////////////////////////////
+
 function initMap() {
 
     var map_document = document.getElementById('map-canvas');
 
     var map_options = {
         zoom: 15,
+        maxZoom: 15,
+        minZoom: 12,
         center: austin,
         /*gestureHandling: "none",*/
         zoomControl: false,
@@ -101,11 +101,16 @@ function initMap() {
                 lng: position.coords.longitude
             };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
+            //infoWindow.setPosition(pos);
+            //infoWindow.setContent('Location found.');
+            //infoWindow.open(map);
             map.setCenter(pos);
             map.setZoom(15);
+            //map.fitBounds(bounds);
+            var listener = google.maps.event.addListener(map, "idle", function () {
+                if (map.getZoom() > 16) map.setZoom(16);
+                google.maps.event.removeListener(listener);
+            });
 
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
@@ -114,27 +119,11 @@ function initMap() {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
-
-
     initPanel(map);
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
+//////////////////////////////////////////////////
 
-            infoWindow.addListener("mouseover", function () {
-
-        infoWindow.open(map, this);
-    })
-    }
-
-    // end of test
-
-
-// Initializes the sidebar panel
 function initPanel(map) {
     var panelDiv = document.getElementById('panel');
 
@@ -142,29 +131,48 @@ function initPanel(map) {
 
     var view = new storeLocator.View(map, data, {
         geolocation: true,
-        features: data.getFeatures(),
     })
 
     var markerSize = new google.maps.Size(24, 24);
     view.createMarker = function (store) {
-        return new google.maps.Marker({
+        let marker = new google.maps.Marker({
             position: store.getLocation(),
-
-
-
         });
 
+        markers.push(marker);
 
+        return marker;
     };
 
     //////////////////////////////////////////////////
 
-    new storeLocator.Panel(panelDiv, {
+    var panel = new storeLocator.Panel(panelDiv, {
         view: view,
         featureFilter: true
     });
+
+    function refreshMarkers() {
+        view.clearMarkers();
+        view.refreshView();
+        panel.setView(view);
+    }
+    $("#beer-check").on("click", refreshMarkers);
+    $("#wine-check").on("click", refreshMarkers);
+    $("#cocktails-check").on("click", refreshMarkers);
+    $("#food-check").on("click", refreshMarkers);
+    $("#hookah-check").on("click", refreshMarkers);
+    $("#dog-check").on("click", refreshMarkers);
+
 }
 
+//////////////////////////////////////////////////
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+}
 
 //////////////////////////////////////////////////
 
@@ -175,68 +183,71 @@ function PlacesDataSource(map) {
 
 //////////////////////////////////////////////////
 
-// Customized and created originalGenerateFieldsHTML with a new function that creates a new and prepends another div 
-storeLocator.Store.prototype.originalGenerateFieldsHTML_ = storeLocator.Store.prototype.generateFieldsHTML_;
-storeLocator.Store.prototype.generateFieldsHTML_ = function (x) {
+storeLocator.Store.prototype.getInfoWindowContent = function (x) {
 
-    fieldsHTML = this.originalGenerateFieldsHTML_(x);
+    var infoHTML = "<div class='store'>";
+
     if (this.props_.picture) {
-        fieldsHTML = '<div class="picture"><img src="' + this.props_.picture + '"></div>' + fieldsHTML;
+        infoHTML = infoHTML + '<div class="picture"><img src="' + this.props_.picture + '"></div>';
     }
     if (this.props_.price_level) {
-        fieldsHTML = fieldsHTML + '<div class="prices">' + this.props_.price_level + '</div>';
+        infoHTML = infoHTML + '<div class="prices">' + this.props_.price_level + '</div>';
+    }
+    if (this.props_.title) {
+        infoHTML = infoHTML + '<div class="title">' + this.props_.title + '</div>';
     }
     if (this.props_.times) {
-        fieldsHTML = fieldsHTML + '<div class="times">' + this.props_.times + '</div>';
+        infoHTML = infoHTML + '<div class="times">' + this.props_.times + '</div>';
     }
 
+    if (this.props_.website) {
+        infoHTML = infoHTML + '<div class="website">' + this.props_.website + '</div>';
+    }
+    if (this.props_.address) {
+        infoHTML = infoHTML + '<div class="address">' + this.props_.address + '</div>';
+    }
+
+    if (this.props_.phone) {
+        infoHTML = infoHTML + '<div class="phone">' + this.props_.phone + '</div>';
+    }
+
+
     if (this.props_.hours) {
-        fieldsHTML = fieldsHTML + '<div class="hours">' + this.props_.hours + '</div>';
+        infoHTML = infoHTML + '<div class="hours">' + this.props_.hours + '</div>';
     }
     if (this.props_.featureList) {
         featuresHTML = '';
 
         if (this.props_.featureList.beer) {
-            featuresHTML += '<p><b>BEER</b></p>';
+            featuresHTML += '<div class="beer">BEER</div>';
         }
         if (this.props_.featureList.wine) {
-            featuresHTML += '<p><b>WINE</b></p>';
+            featuresHTML += '<div class="wine">WINE</div>';
         }
         if (this.props_.featureList.cocktails) {
-            featuresHTML += '<p><b>Cocktails</b></p>';
+            featuresHTML += '<div class="cocktails">Cocktails</div>';
         }
         if (this.props_.featureList.food) {
-            featuresHTML += '<p><b>FOOD</b></p>';
+            featuresHTML += '<div class="food">FOOD</div>';
         }
         if (this.props_.featureList.hookah) {
-            featuresHTML += '<p><b>HOOKAH</b></p>';
+            featuresHTML += '<div class="hookah">HOOKAH</div>';
         }
         if (this.props_.featureList.dog) {
-            featuresHTML += '<p><b>DOG</b></p>';
+            featuresHTML += '<div class="dog">DOG</div>';
         }
-        fieldsHTML = fieldsHTML + '<div class="featurelist">' + featuresHTML + '</div>';
+        infoHTML = infoHTML + '<div class="featurelist">' + featuresHTML + '</div>';
     }
-    return fieldsHTML;
+    return infoHTML + "</div>";
 
 }
 
-// check if features.length = 0 if not no firebase if is greater, then look it up in firebase
-// firebase.database 
-
+//////////////////////////////////////////////////
 
 PlacesDataSource.prototype.getStores = function (bounds, features, callback) {
     var service = this.service_;
     var details_cache = this.details_cache_;
     var database = firebase.database();
-
-    var beerCall = this.FEATURES_.getById('Beer-YES');
-    var wineCall = this.FEATURES_.getById('Wine-YES');
-    var foodCall = this.FEATURES_.getById('Food-YES');
-    var hookahCall = this.FEATURES_.getById('Hookah-YES');
-    var dogCall = this.FEATURES_.getById('Dog-YES')
-
-       // props.features with an array FEATURES_
-
 
     service.search({
         bounds: bounds,
@@ -252,8 +263,93 @@ PlacesDataSource.prototype.getStores = function (bounds, features, callback) {
 
         var callbacksRemaining = results.length;
 
-        for (var i = 0; results[i]; i++) {
-            var result = results[i];
+        function detailsCallback(details, details_status, snapshot, result) {
+            console.log(result);
+
+            if (details && details_status != 'CACHED') {
+                details_cache[result.place_id] = details;
+            }
+
+            var props = {
+                title: result.name,
+                price_level: result.price_level,
+                address: result.vicinity,
+                types: result.types,
+                //icon: result.icon,
+                hours: result.opening_hours,
+                website: result.website,
+
+            };
+
+            if (result.price_level) {
+                if (result.price_level == "1" || result.price_level == "0") {
+                    props.price_level = "$";
+                } else if (result.price_level == "2") {
+                    props.price_level = "$$";
+                } else if (result.price_level == "3") {
+                    props.price_level = "$$$";
+                } else {
+                    props.price_level = "$$$$";
+                }
+            } else {
+                props.price_level = "";
+            }
+
+            if (details) {
+                if (details.formatted_phone_number) {
+                    props.phone = details.formatted_phone_number;
+                } else if (details.international_phone_number) {
+                    props.phone = details.international_phone_number;
+                }
+            }
+
+            if (result.photos) {
+                props.picture = result.photos[0].getUrl({ 'maxWidth': 100, 'maxHeight': 100 });
+            }
+
+            if (result.opening_hours) {
+                if (result.opening_hours.open_now == true) {
+                    props.hours = "Open Now";
+                } else if (result.opening_hours.open_now == false) {
+                    props.hours = "Closed Now";
+                }
+            }
+
+            var features = {};
+            if (snapshot.val() !== null) {
+                props.times = snapshot.val().times.starttime + " - " + snapshot.val().times.endtime;
+                props.featureList = snapshot.val().features;
+                features = snapshot.val().features;
+            }
+
+            if (matchFeatures(features)) {
+                var store = new storeLocator.Store(result.id, result.geometry.location, null, props);
+                stores.push(store);
+            } else {
+                console.log("skipping '" + props.title + "' due to missing feature");
+            }
+
+            if (--callbacksRemaining <= 0) {
+                console.log('invoking stores callback');
+                callback(stores);
+            }
+        }
+
+        function databaseCallback(snapshot, result) {
+            if (details_cache[result.place_id]) {
+                detailsCallback(details_cache[result.place_id], 'CACHED', snapshot, result);
+            } else {
+                var request = {
+                    placeId: result.place_id
+                };
+                service.getDetails(request, function (details, details_status) {
+                    detailsCallback(details, details_status, snapshot, result)
+                });
+            }
+        }
+
+        for (let result of results) {
+            console.log(result);
 
             if (!result) {
                 if (--callbacksRemaining <= 0) {
@@ -263,116 +359,28 @@ PlacesDataSource.prototype.getStores = function (bounds, features, callback) {
                 continue;
             }
 
-            function detailsCallback(details, details_status, snapshot) {
-                if (details && details_status != 'CACHED') {
-                    details_cache[result.place_id] = details;
-                }
-
-                var props = {
-                    title: result.name,
-                    address: result.vicinity,
-                    types: result.types,
-                    //icon: result.icon,
-                    hours: result.opening_hours,
-                    price_level: result.price_level
-
-                };
-
-                if (result.price_level) {
-                    if (result.price_level == "1" || result.price_level == "0") {
-                        props.price_level = "$";
-                    } else if (result.price_level == "2") {
-                        props.price_level = "$$";
-                    } else if (result.price_level == "3") {
-                        props.price_level = "$$$";
-                    } else {
-                        props.price_level = "$$$$";
-                    }
-                } else {
-                    props.price_level = "This location does not provide price level service ";
-                }
-
-                if (details) {
-                    props.phone = details.formatted_phone_number;
-                }
-
-                if (result.photos) {
-                    props.picture = result.photos[0].getUrl({ 'maxWidth': 100, 'maxHeight': 100 });
-                }
-
-                if (result.opening_hours) {
-                    if (result.opening_hours.open_now == true) {
-                        props.hours = "It is open now";
-                    } else if (result.opening_hours.open_now == false) {
-                        props.hours = "It is closed now";
-                    }
-                }
-
-                if (snapshot.val() !== null) {
-                    props.times = snapshot.val().times.starttime + " - " + snapshot.val().times.endtime;
-                    props.featureList = snapshot.val().features;
-                }
-
-                var store = new storeLocator.Store(result.id, result.geometry.location, null, props);
-
-                stores.push(store);
-
-                if (--callbacksRemaining <= 0) {
-                    console.log('invoking stores callback');
-                    callback(stores);
-                }
-            }
-
-            function databaseCallback(snapshot) {
-                if (details_cache[result.place_id]) {
-                    detailsCallback(details_cache[result.place_id], 'CACHED', snapshot);
-                } else {
-                    var request = {
-                        placeId: result.place_id
-                    };
-                    service.getDetails(request, function (details, details_status) {
-                        detailsCallback(details, details_status, snapshot)
-                    });
-                }
-            }
-
-            database.ref("/happyHowlerData/places").child(result.place_id).once("value", databaseCallback);
+            database.ref("/happyHowlerData/places").child(result.place_id).once("value", function (snapshot) {
+                databaseCallback(snapshot, result);
+            });
         }
     });
 };
 
-
-
-// Test   
-/*
-storeLocator.View.prototype.highlight().hover(
-function () {
-
-    
-}
-
-);*/
-
-
 //////////////////////////////////////////////////
 
-PlacesDataSource.prototype.FEATURES_ = new storeLocator.FeatureSet(
-    new storeLocator.Feature('Beer-YES', 'Beer'),
-    new storeLocator.Feature('Wine-YES', 'Wine'),
-    new storeLocator.Feature('Cocktails-YES', 'Cocktails'),
-    new storeLocator.Feature('Food-YES', 'Food'),
+function matchFeatures(features) {
+    if (($("#beer-check").prop("checked") && !features.beer) ||
+        ($("#wine-check").prop("checked") && !features.wine) ||
+        ($("#cocktails-check").prop("checked") && !features.cocktails) ||
+        ($("#food-check").prop("checked") && !features.food) ||
+        ($("#hookah-check").prop("checked") && !features.hookah) ||
+        ($("#dog-check").prop("checked") && !features.dog)) {
+        return false;
+    } else {
+        return true;
+    }
 
-    new storeLocator.Feature('Hookah-YES', 'Hookah'),
-    new storeLocator.Feature('Dog-YES', 'Dog')
-
-);
-
-PlacesDataSource.prototype.getFeatures = function () {
-    return this.FEATURES_;
-};
-
-
-// Test
-
+}
 
 google.maps.event.addDomListener(window, 'load', initMap);
+
