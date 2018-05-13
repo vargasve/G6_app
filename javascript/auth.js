6f16bb775080bd2c6fa44b9cc05881d887f62625
Technophile <patricia.snodgrass@gmail.com>
2018-05-12 21:17:28 -0500

    merge

--- javascript/auth.js
+++ javascript/auth.js
@@ -16,7 +16,6 @@ function toggleSignIn() {
 
 }
 
-
 /**
  * initApp handles setting up UI event listeners and registering Firebase auth listeners:
  *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
@@ -31,35 +30,31 @@ function initApp() {
         if (result.credential) {
             // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
-            // [START_EXCLUDE]
             document.getElementById('quickstart-account-details').textContent = token.displayName;
         } else {
             document.getElementById('quickstart-account-details').textContent = 'null';
-            // [END_EXCLUDE]
         }
         // The signed-in user info.
         var user = result.user;
     }).catch(function (error) {
-        // Handle Errors here.
+
         var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
+
         var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
+
         var credential = error.credential;
-        // [START_EXCLUDE]
+
         if (errorCode === 'auth/account-exists-with-different-credential') {
             alert('You have already signed up with a different auth provider for that email.');
-            // If you are using multiple auth providers on your app you should handle linking
-            // the user's accounts here.
+
+            
             console.error(error);
         }
-        // [END_EXCLUDE]
-    });
-    // [END getidptoken]
 
-    // Listening for auth state changes.
-    // [START authstatelistener]
+    });
+
+
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
@@ -70,7 +65,7 @@ function initApp() {
             var isAnonymous = user.isAnonymous;
             var uid = user.uid; // Use this key to store favorites
             var providerData = user.providerData;
-            // [START_EXCLUDE]
+
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed in as' + displayName;
             $("#quickstart-sign-in").text('Sign out');
             $("#quickstart-sign-in").addClass('qs-sign-out');
@@ -80,10 +75,9 @@ function initApp() {
             document.getElementById('quickstart-account-details').textContent = displayName;
             document.getElementById('quickstart-avatar').innerHTML = "<img src='" + photoURL + "'/>";
 
-            // [END_EXCLUDE]
+
         } else {
-            // User is signed out.
-            // [START_EXCLUDE]
+
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
             document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
             document.getElementById('quickstart-account-details').textContent = 'null';
